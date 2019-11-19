bc65f5055a5537268c64fa17fe1d5462057ff3a3
dan <chronos937@gmail.com>
2019-11-19 18:33:20 -0500

    added seed file

---
+++ bamazon-seed.sql
@@ -0,0 +1,18 @@
+drop database if exists bamazon;
+create database bamazon;
+use bamazon;
+
+-- creates inventory table
+create table inventory (
+    item_id int not null,
+    product_name varchar (40) not null,
+    department_name varchar (40),
+    price int (10, 2),
+    stock_quantity int not null,
+    primary key (item_id)
+)
+
+-- populates inventory
+insert into inventory (item_id,product_name,department_name,price,stock_quantity){
+    value 
+}

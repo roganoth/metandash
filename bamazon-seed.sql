cb80a9d5179764392c8d3167fe2432159e8af3ba
dan demeo <chronos937@gmail.com>
2019-11-21 03:30:34 -0500

    updated seed file with items for the inventory table

--- bamazon-seed.sql
+++ bamazon-seed.sql
@@ -4,14 +4,24 @@ use bamazon;
 
 -- creates inventory table
 create table inventory (
-    item_id int not null,
+    item_id int auto_increment not null,
     product_name varchar (40) not null,
     department_name varchar (40),
-    price int (10, 2),
+    price decimal (10, 2),
     stock_quantity int not null,
     primary key (item_id)
 )
 
 -- populates inventory
-insert into inventory (item_id,product_name,department_name,price,stock_quantity)
-    values (1, "sample item", "sample department", 10.02, 1)
+insert into inventory (product_name,department_name,price,stock_quantity)
+    values 
+        ("Tennis Racket", "Sporting Goods", 149.99, 100),
+        ("Tennis Shoes", "Sporting Goods", 75, 50),
+        ("Bowling Ball(16lbs)", "Sporting Goods", 159.99, 5),
+        ("Xbox One Elite Controller", "Video Games", 149.99, 3),
+        ("Nintento Switch", "Video Games", 299.99, 50),
+        ("PS4 Pro", "Video Games", 399.99, 10),
+        ("Pokemon Sword", "Video Games", 59.99, 50),
+        ("Cheerios(Chocolate)", "Grocery", 3.49, 3),
+        ("D&D Rick and Morty Box", "Board Games", 29.99, 1),
+        ("Reign", "Grocery", 2.49, 3);
